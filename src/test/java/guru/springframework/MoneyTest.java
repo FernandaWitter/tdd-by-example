cf205b5a3b6631bddd090df252d93e24e340eca4
Fernanda I B Witter <fernanda.witter@yahoo.com>
2020-06-22 16:44:08 -0300

    feat: creates first tests

--- src/test/java/guru/springframework/MoneyTest.java
+++ src/test/java/guru/springframework/MoneyTest.java
@@ -1,4 +1,22 @@
 package guru.springframework;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class MoneyTest {
+
+    @Test
+    void testMultiplicationDollar(){
+        Dollar five = new Dollar(5);
+        Dollar product = five.times(2);
+        Assertions.assertEquals(new Dollar(10),  product);
+        product = five.times(3);
+        Assertions.assertEquals(new Dollar(15), product);
+    }
+
+    @Test
+    void testEqualityDollar(){
+        Assertions.assertEquals(new Dollar(5), new Dollar(5));
+        Assertions.assertNotEquals(new Dollar(5), new Dollar(8));
+    }
 }
