0003a45ccde43de18b02d5821d94040faabee6f5
Fernanda I B Witter <fernanda.witter@yahoo.com>
2020-06-22 16:45:13 -0300

    feat: adds Franc tests

--- src/test/java/guru/springframework/MoneyTest.java
+++ src/test/java/guru/springframework/MoneyTest.java
@@ -19,4 +19,18 @@ public class MoneyTest {
         Assertions.assertEquals(new Dollar(5), new Dollar(5));
         Assertions.assertNotEquals(new Dollar(5), new Dollar(8));
     }
+
+    @Test
+    void testMultiplicationFranc(){
+        Franc five = new Franc(5);
+        Franc product = five.times(2);
+        Assertions.assertEquals(new Franc(10),  product);
+        product = five.times(3);
+        Assertions.assertEquals(new Franc(15), product);
+    }
+
+    @Test
+    void testEqualityFranc(){
+        Assertions.assertEquals(new Franc(5), new Franc(5));
+    }
 }
