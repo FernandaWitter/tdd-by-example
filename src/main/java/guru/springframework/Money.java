caf9dbc82a8d3fe0236405da470b7861348d6ead
Fernanda I B Witter <fernanda.witter@yahoo.com>
2020-07-02 15:13:31 -0300

    refactor: removes redundant subclasses

--- src/main/java/guru/springframework/Money.java
+++ src/main/java/guru/springframework/Money.java
@@ -10,8 +10,8 @@ public class Money {
         this.currency = currency;
     }
 
-    public static Money dollar(int amount){return new Dollar(amount, "USD");}
-    public static Money franc(int amount){return new Franc(amount, "CHF");}
+    public static Money dollar(int amount){return new Money(amount, "USD");}
+    public static Money franc(int amount){return new Money(amount, "CHF");}
 
     public Money times(int multiplier){
         return new Money(this.amount*multiplier, this.currency);
--- src/test/java/guru/springframework/MoneyTest.java
+++ src/test/java/guru/springframework/MoneyTest.java
@@ -6,28 +6,21 @@ import org.junit.jupiter.api.Test;
 public class MoneyTest {
 
     @Test
-    void testMultiplicationDollar(){
-        Money five = Money.dollar(5);
-        Assertions.assertEquals(Money.dollar(10),  five.times(2));
-        Assertions.assertEquals(Money.dollar(15), five.times(3));
+    void testMultiplication(){
+        Money fiveDollars = Money.dollar(5);
+        Assertions.assertEquals(Money.dollar(10), fiveDollars.times(2));
+        Assertions.assertEquals(Money.dollar(15), fiveDollars.times(3));
+
+        Money fiveFrancs = Money.franc(5);
+        Assertions.assertEquals(Money.franc(10), fiveFrancs.times(2));
+        Assertions.assertEquals(Money.franc(15), fiveFrancs.times(3));
     }
 
     @Test
-    void testEqualityDollar(){
+    void testEquality(){
         Assertions.assertEquals(Money.dollar(5), Money.dollar(5));
         Assertions.assertNotEquals(Money.dollar(5), Money.dollar(8));
         Assertions.assertNotEquals(Money.dollar(5), Money.franc(5));
-    }
-
-    @Test
-    void testMultiplicationFranc(){
-        Money five = Money.franc(5);
-        Assertions.assertEquals(Money.franc(10), five.times(2));
-        Assertions.assertEquals(Money.franc(15), five.times(3));
-    }
-
-    @Test
-    void testEqualityFranc(){
         Assertions.assertEquals(Money.franc(5), Money.franc(5));
         Assertions.assertNotEquals(Money.franc(5), Money.franc(8));
         Assertions.assertNotEquals(Money.franc(5), Money.dollar(5));
